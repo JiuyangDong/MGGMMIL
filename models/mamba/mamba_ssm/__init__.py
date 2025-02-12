dfbe91120e0c7c822abdbbba758c307ddabd2826
JiuyangDong <110771682+JiuyangDong@users.noreply.github.com>
2025-02-12 18:16:54 +0800

    Update __init__.py

--- models/mamba/mamba_ssm/__init__.py
+++ models/mamba/mamba_ssm/__init__.py
@@ -1,6 +1,6 @@
 __version__ = "1.1.2"
 import sys
-sys.path.append('../../01.codes/04.MGGMMIL/models/')
+sys.path.append('../../01.codes/MGGMMIL/models/')
 
 # from mamba.mamba_ssm.ops.selective_scan_interface import selective_scan_fn, mamba_inner_fn
 from mamba.mamba_ssm.modules.mamba_simple import Mamba
